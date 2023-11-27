1c49aa8982e2df945814a44dab6d583e327cfa20
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-11-27 01:04:43 -0500

    Uploading Effects Lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
+  console.log(timeRemaining)
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        // Reset to 10 when the timer reaches 0
+        setTimeRemaining(10);
+        onAnswered(false)
+      }
+    }, 1000);
+
+    return function cleanUp() {
+      clearInterval(timer)
+    }
+  }, [timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
